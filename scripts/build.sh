f7d49f802e7a3f01f541a165e6b52228ea7d75ab
Jay <jay@altiscale.com>
2014-03-04 11:11:06 -0800

    dependencies hell

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,7 +19,7 @@ mkdir -p ${RPM_DIR}
 #wget setup
 WGET_OPTS="--progress=dot:mega -S -T 10 -t 5"
 
-ant_link='vcc-apache-ant-1.8.4-201301271707.x86_64.rpm'
+ant_package='vcc-apache-ant-1.8.4-201301271707.x86_64.rpm'
 asciidoc_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/asciidoc-8.4.5-4.1.el6.noarch.rpm'
 #cyrus-sasl-devel_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/cyrus-sasl-devel-2.1.23-13.el6_3.1.x86_64.rpm'
 #cyrus-sasl-gssapi_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/cyrus-sasl-gssapi-2.1.23-13.el6_3.1.x86_64.rpm'
@@ -38,7 +38,7 @@ python_simplejson_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/py
 #mysql-devel_link=''
 
 
-wget ${WGET_OPTS} https://Et_Omwyfs4:Rez1Shrik5@yum.service.verticloud.com/prod/${ant_link} -O ${RPM_DIR}/${ant_link}
+wget ${WGET_OPTS} https://Et_Omwyfs4:Rez1Shrik5@yum.service.verticloud.com/prod/${ant_package} -O ${RPM_DIR}/${ant_package}
 wget ${WGET_OPTS} ${asciidoc_link} -O ${RPM_DIR}/asciidoc-8.4.5-4.1.el6.noarch.rpm
 wget ${WGET_OPTS} ${python_devel_link} -O ${RPM_DIR}/python-devel-2.6.6-51.el6.x86_64.rpm
 wget ${WGET_OPTS} ${python_simplejson_link} -O ${RPM_DIR}/python-simplejson-2.0.9-3.1.el6.x86_64.rpm
@@ -46,5 +46,7 @@ wget ${WGET_OPTS} ${python_simplejson_link} -O ${RPM_DIR}/python-simplejson-2.0.
 #RPM Install
 rpm --initdb --dbpath ${RPM_DB_DIR}
 #rpm -ivh ant asciidoc cyrus-sasl-devel cyrus-sasl-gssapi libtidy libxml2-devel libxslt-devel openldap-devel python-devel python-simplejson sqlite-devel krb5-devel gcc gcc-c++ mysql mysql-devel cyrus-sasl-plain --prefix ${INSTALL_DIR} --dbpath ${RPM_DB_DIR} --nodeps --quiet
-rpm -ivh ant asciidoc python-devel python-simplejson --prefix ${INSTALL_DIR} --dbpath ${RPM_DB_DIR} --nodeps --quiet
+rpm -ivh ${RPM_DIR}/${ant_package} ${RPM_DIR}/asciidoc-8.4.5-4.1.el6.noarch.rpm ${RPM_DIR}/python-devel-2.6.6-51.el6.x86_64.rpm ${RPM_DIR}/python-simplejson-2.0.9-3.1.el6.x86_64.rpm --prefix ${INSTALL_DIR} --dbpath ${RPM_DB_DIR} --nodeps --quiet
 
+export SKIP_PYTHONDEV_CHECK=true
+export PYTHON_H=
