1e59b69aba7ffd33c8fff95c5616df4d3ca47f7d
Jay <jay@altiscale.com>
2014-03-04 10:57:55 -0800

    dependencies hell

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,8 +25,8 @@ asciidoc_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/asciidoc-8.
 #libxml2-devel_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/libxml2-devel-2.7.6-14.el6.x86_64.rpm'
 #libxslt-devel_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/libxslt-devel-1.1.26-2.el6_3.1.x86_64.rpm'
 #openldap-devel_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/openldap-devel-2.4.23-32.el6_4.1.x86_64.rpm'
-python-devel_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/python-devel-2.6.6-51.el6.x86_64.rpm'
-python-simplejson_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/python-simplejson-2.0.9-3.1.el6.x86_64.rpm'
+python_devel_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/python-devel-2.6.6-51.el6.x86_64.rpm'
+python_simplejson_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/python-simplejson-2.0.9-3.1.el6.x86_64.rpm'
 #sqlite-devel_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/sqlite-devel-3.6.20-1.el6.x86_64.rpm'
 #krb5-devel_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/krb5-devel-1.10.3-10.el6_4.6.x86_64.rpm'
 #cyrus-sasl-plain_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/cyrus-sasl-plain-2.1.23-13.el6_3.1.x86_64.rpm'
@@ -37,9 +37,9 @@ python-simplejson_link='ftp://rpmfind.net/linux/centos/6.5/os/x86_64/Packages/py
 
 
 wget ${WGET_OPTS} https://Et_Omwyfs4:Rez1Shrik5@yum.service.verticloud.com/prod/${ant_link} -O ${RPM_DIR}/${ant_link}
-wget ${WGET_OPTS} ${asciidoc_link} -O "${RPM_DIR}/asciidoc-8.4.5-4.1.el6.noarch.rpm"
-wget ${WGET_OPTS} ${python-devel_link} -O "${RPM_DIR}/python-devel-2.6.6-51.el6.x86_64.rpm"
-wget ${WGET_OPTS} ${python-simplejson_link} -O "${RPM_DIR}/python-simplejson-2.0.9-3.1.el6.x86_64.rpm"
+wget ${WGET_OPTS} ${asciidoc_link} -O ${RPM_DIR}/asciidoc-8.4.5-4.1.el6.noarch.rpm
+wget ${WGET_OPTS} ${python_devel_link} -O ${RPM_DIR}/python-devel-2.6.6-51.el6.x86_64.rpm
+wget ${WGET_OPTS} ${python_simplejson_link} -O ${RPM_DIR}/python-simplejson-2.0.9-3.1.el6.x86_64.rpm
 
 #RPM Install
 rpm --initdb --dbpath ${RPM_DB_DIR}
